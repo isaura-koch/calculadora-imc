bb409193782013a09f6c46a3ebf806afff02fcf9
Isaura <isahhhkoch@gmail.com>
2019-10-27 16:23:25 -0300

    Update imc.py
    Adicionando interface e corrigindo pep8

--- imc.py
+++ imc.py
@@ -1,12 +1,25 @@
 def calculaIMC(altura, peso):
-    dobroAltura = altura * altura
-    totalIMC = peso/dobroAltura
-    if totalIMC < 18.5:
+    """Função responsável por calcular o IMC"""
+    dobro_altura = altura * altura
+    total_imc = peso/dobro_altura
+
+    if total_imc < 18.5:
         resultado = "Você está abaixo do peso!"
-    elif totalIMC >= 18.5 and totalIMC <= 24.9:
+    elif total_imc >= 18.5 and total_imc <= 24.9:
         resultado = "Você está com o peso ideal!"
-    elif totalIMC > 25 and totalIMC <= 29.9:
+    elif total_imc > 25 and total_imc <= 29.9:
         resultado = "Você está acima do peso!"
-    else: 
-	    resultado = "Você está obeso!"
+    else:
+        resultado = "Você está obeso!"
+
     return resultado
+
+
+try:
+    peso_em_kg = int(input("Insira seu peso: "))
+    altura_em_metros = float(input("Insira sua altura em metros: "))
+except:
+    print("Por favor insira seus dados corretamente!")
+
+imc = calculaIMC(altura_em_metros, peso_em_kg)
+print(imc)
